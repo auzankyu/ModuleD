cd2ace2ef8adc4eb1daea7d2d3c0aaefdb730bdb
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-03-02 11:30:54 +0700

    Initial Commit

---
+++ ModuleD/ModuleD.h
@@ -0,0 +1,18 @@
+//
+//  ModuleD.h
+//  ModuleD
+//
+//  Created by Auzan Lazuardi on 02/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleD.
+FOUNDATION_EXPORT double ModuleDVersionNumber;
+
+//! Project version string for ModuleD.
+FOUNDATION_EXPORT const unsigned char ModuleDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleD/PublicHeader.h>
+
+
