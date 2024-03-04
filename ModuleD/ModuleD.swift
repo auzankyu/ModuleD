3f3b7b7923a2cb5107e0a2944d0b073291f52c8e
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-03-04 11:09:03 +0700

    initial commit Module D

---
+++ ModuleD/ModuleD.swift
@@ -0,0 +1,18 @@
+//
+//  ModuleD.swift
+//  ModuleD
+//
+//  Created by Auzan Lazuardi on 02/03/24.
+//
+
+import Foundation
+import AppCore
+
+public class ModuleD {
+    public static let shared = ModuleD()
+    
+    public func getData() {
+        let app = AppCore()
+        app.start()
+    }
+}
